002b49ee426c5faa4fcfedba5deca1821741f8dc
patel-param-09 <patelparam9304@gmail.com>
2024-09-12 17:16:11 +0530

    Some Changes in Cart page

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -5,7 +5,7 @@ import { assets } from "../assets/assets";
 import CartTotal from "../components/CartTotal";
 
 const Cart = () => {
-  const { products, currency, cartItems, updateQuantity } =
+  const { products, currency, cartItems, updateQuantity, navigate } =
     useContext(ShopContext);
   const [cartData, setCartData] = useState([]);
 
@@ -92,6 +92,20 @@ const Cart = () => {
       <div className="flex justify-end my-20">
         <div className="w-full sm:w-[450px]">
           <CartTotal />
+          <div className="w-full text-end">
+            {cartData != "" ? (
+              <button
+                className="bg-black text-white text-sm my-8 px-8 py-3"
+                onClick={() => {
+                  navigate("/place-order");
+                }}
+              >
+                PROCEED TO CHECKOUT
+              </button>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
       </div>
     </div>
